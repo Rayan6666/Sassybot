735504783efb397860ce5375a29d2aa71a9f32e5
Rayan6666 <baskruithof75@gmail.com>
2019-06-13 16:47:45 +0200

    Commit 3: Added slap, fixed verify, added user-purge.

--- bot.js
+++ bot.js
@@ -3,67 +3,110 @@ const discord = require('discord.js');
 const fs = require('fs');
 const Client = new discord.Client();
 Client.commands = new discord.Collection();
+try {
 
-Client.on('ready', async () => {
-    console.log("Sassy 3.0 loaded, Instantiating command files..");
-    var channel = Client.channels.get('253612214148136981');
-    channel.sendMessage("Sassy has been restarted!").then(msg => {
-        msg.delete(10000)
-    });
-});
-
-fs.readdir("./commands/", (err, files) => {
-
-    if(err){
-        console.log(err);
-    }
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0){
-        console.log("Couldn't find commands. Killing bot");
-        return;
-    }
-
-    jsfile.forEach((f, i) =>{
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded!`);
-        Client.commands.set(props.help.name, props);
+    Client.on('ready', async () => {
+        console.log("Sassy 3.0 loaded, Instantiating command files..");
+        var channel = Client.channels.get('253612214148136981');
+        channel.send("Sassy has been restarted!").then(msg => {
+            msg.delete(10000)
+        });
     });
 
-});
+    fs.readdir("./commands/", (err, files) => {
 
-fs.readdir("./commands/roles/", (err, files) => {
+        if(err){
+            console.log(err);
+        }
 
-    if(err){
-        console.log(err);
-    }
+        let jsfile = files.filter(f => f.split(".").pop() === "js");
+        if(jsfile.length <= 0){
+            console.log("Couldn't find commands. Killing bot");
+            return;
+        }
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0){
-        console.log("Couldn't find roles. Killing bot");
-        return;
-    }
+        jsfile.forEach((f, i) => {
+            let props = require(`./commands/${f}`);
+            console.log(`${f} loaded!`);
+            Client.commands.set(props.help.name, props);
+        });
 
-    jsfile.forEach((f, i) =>{
-        let props = require(`./commands/roles/${f}`);
-        console.log(`${f} loaded!`);
-        Client.commands.set(props.help.name, props);
     });
 
-});
+    fs.readdir("./commands/roles/", (err, files) => {
+
+        if(err){
+            console.log(err);
+        }
+
+        let jsfile = files.filter(f => f.split(".").pop() === "js");
+        if(jsfile.length <= 0){
+            console.log("Couldn't find roles. Killing bot");
+            return;
+        }
+
+        jsfile.forEach((f, i) =>{
+            let props = require(`./commands/roles/${f}`);
+            console.log(`${f} loaded!`);
+            Client.commands.set(props.help.name, props);
+        });
+
+    });
 
 
-Client.on('message', async (message) => {
-    const prefix = config.prefix;
-    const messageArray = message.content.split(" ");
-    const command = messageArray[0];
-    const argument = message.content.slice(prefix.length).split(' ');
-    var server = message.guild.id;
-    var memberCount = Client.guilds.get(server).memberCount;
-    console.log(memberCount, server);
+    Client.on('message', async (message) => {
+        const prefix = config.prefix;
+        const messageArray = message.content.split(" ");
+        const command = messageArray[0];
+        const argument = message.content.slice(prefix.length).split(' ');
+        var server = message.guild.id;
+        var memberCount = Client.guilds.get(server).memberCount;
+        console.log(memberCount, server);
 
-    let commandfile = Client.commands.get(command.slice(config.prefix.length));
-    if(commandfile) commandfile.run(Client, message, argument);
-});
+        let commandfile = Client.commands.get(command.slice(config.prefix.length));
+        if(commandfile) commandfile.run(Client, message, argument);
 
-Client.login(config.token);
+        if (message.guild === null) {
+            return message.author.send("Fuck off you cunt");
+        }
+
+        if (messageArray[0].startsWith(`${config.prefix}slap`))
+        {
+            if (message.member.hasPermission("KICK_MEMBERS"))
+            {
+                try {
+                    let target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
+                    if (target) {
+                        await message.channel.send(target + " was slapped!", {
+                            file: 'giphy.gif'
+                        }).then(msg => {
+                            msg.delete(25000);
+                        });
+                        return await target.addRole(message.guild.roles.find(role => role.name === "Muted"));
+                    }
+                } catch (e) {
+                    return await console.log("An error occured while running slap!", chalk.red(e))
+                }
+            }
+            return await message.author.send("You do not have permission to do this.");
+        }
+
+    });
+
+    Client.on('disconnect', async (message) => {
+        var channel = Client.channels.get('253612214148136981');
+        try {
+            channel.send("It appears that sassy has crashed, attempting to auto restart...");
+            Client.destroy();
+            Client.login(config.token);
+        } catch (e) {
+            console.log(e);
+            process.exit(1);
+        }
+    });
+
+    Client.login(config.token);
+
+} catch (e) {
+    return console.log(chalk.red("An error occured!", e))
+}
--- commands/admin.js
+++ commands/admin.js
@@ -37,6 +37,82 @@ module.exports.run = async (Client, message, args) => {
             }
         }
 
+        if (args[1] === "user-purge") {
+            let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
+
+            if (!user) {
+                return message.channel.send("You did not specify an user to purge messages from.").then(message => {
+                    message.delete(3500)
+                });
+            }
+            if (!args[2]) {
+                return message.channel.send("You did not specify an amount to purge.").then(message => {
+                    message.delete(3500);
+                });
+            }
+            if (isNaN(args[3])) {
+                return message.channel.send("Amount must be an integer. You gave " + args[3]).then(message => {
+                    message.delete(3500);
+                });
+            }
+            if (user.hasPermission("KICK_MEMBERS")) {
+                return message.channel.send("You can not purge messages sent by staff.").then(message => {
+                    message.delete(3500)
+                });
+            }
+            message.channel.fetchMessages({
+                limit: 100
+            }).then(messages => {
+                let messageFilter = user ? user.id : Client.user.id;
+                messages = messages.filter(message => message.author.id === messageFilter).array().slice(0, args[3]);
+                message.channel.bulkDelete(messages);
+                console.log(args);
+                return message.channel.send("Deletes message(s) from " + user);
+            });
+        }
+
+        if (args[1] === "setrole") {
+            if (message.member.hasPermission("BAN_MEMBERS")) {
+                let target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
+                let name = args[3];
+
+                if (target) {
+                    if (message.guild.roles.find(role => role.name === name)) {
+                        if (target.roles.find(role => role.name === name)) {
+                            target.removeRole(target.roles.find(role => role.name === name));
+                            return message.channel.send("Removed role" + " " + args[3] + " from " + target)
+                        } else {
+                            target.addRole(message.guild.roles.find(role => role.name === name));
+                            return message.channel.send("Added role " + args[3] + " to" + target + "'s" + " roles")
+                        }
+
+                    } else {
+                        return message.channel.send("Role" + " " + args[3] + " not found in roles list.");
+                    }
+                } else {
+                    return message.channel.send("No user specified to give a role to!");
+                }
+
+            } else {
+                return message.channel.send("You do not have permission to assign roles.");
+            }
+        }
+
+        if (args[1] === "quickpin") {
+            if (args[2])
+            {
+                const messageArray = message.content.split("");
+                console.log(messageArray);
+
+                return message.channel.send(args[2]).then((msg) => {
+                    msg.pin().then(message => {
+                        message.delete(1000)
+                    });
+                })
+            }
+            return false;
+        }
+
         if (args[1] === "verify") {
             if (args[2]) {
                 let target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
@@ -57,32 +133,6 @@ module.exports.run = async (Client, message, args) => {
         return message.channel.send("You do not have permission to use admin commands.");
     }
 
-    if (args[1] === "setrole") {
-        if (message.member.hasPermission("BAN_MEMBERS")) {
-            let target = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[2]);
-            let name = args[3];
-
-            if (target) {
-                if (message.guild.roles.find(role => role.name === name)) {
-                    if (target.roles.find(role => role.name === name)) {
-                        target.removeRole(target.roles.find(role => role.name === name));
-                        return message.channel.send("Removed role" + " " + args[3] + " from " + target)
-                    } else {
-                        target.addRole(message.guild.roles.find(role => role.name === name));
-                        return message.channel.send("Added role " + args[3] + " to" + target + "'s" + " roles")
-                    }
-
-                } else {
-                    return message.channel.send("Role" + " " + args[3] + " not found in roles list.");
-                }
-            } else {
-                return message.channel.send("No user specified to give a role to!");
-            }
-
-        } else {
-            return message.channel.send("You do not have permission to assign roles.");
-        }
-    }
 };
 
 module.exports.help = {
